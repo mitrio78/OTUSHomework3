80f0aea439dbf7ab7175c0d9665d33c2774080fa
Grishechko, Dmitriy <ext_dgrishechko@rencredit.ru>
2023-06-30 10:58:18 +0300

    Initial Commit

---
+++ OTUSHomework3/OTUSHomework3App.swift
@@ -0,0 +1,17 @@
+//
+//  OTUSHomework3App.swift
+//  OTUSHomework3
+//
+//  Created by anduser on 30.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct OTUSHomework3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
